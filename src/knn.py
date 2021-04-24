7a99a02bd189cdbf5589d2cbfde9e03dad3d7525
sakdag <serhatakdag96@gmail.com>
2021-04-24 21:17:49 +0300

    knn implementation (unfinished)

--- src/knn.py
+++ src/knn.py
@@ -1,36 +1,82 @@
+import numpy
+import numpy as np
 import pandas
+from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error
+from sklearn.neighbors import DistanceMetric
+from src.main import DistanceMetrics
 
 
-# k_value describes k in k-nn
-# Currently columns_to_use expected to be only numeric columns, others will be added later
-def measure_performance(train_set: pandas.DataFrame, test_set: pandas.DataFrame, k_value: int, columns_to_use: set):
+def measure_performance(df: pandas.DataFrame, k_value: int, number_of_folds: int, distance_metric):
 
     # Calculate similarity between train and test data
     # If its one of k neighbors, add it's productivity to
     # current_neighbors map
 
-    for test_index, test_row in test_set.iterrows():
+    split_dfs = np.array_split(df, number_of_folds)
 
-        current_similarities = dict()
-        predicted_productivity = 0
+    mse_list = list()
+    rmse_list = list()
+    mape_list = list()
 
-        for train_index, train_row in train_set.iterrows():
+    if distance_metric.value == DistanceMetrics.MANHATTAN_DISTANCE.value:
+        dist = DistanceMetric.get_metric('manhattan')
+    else:
+        dist = DistanceMetric.get_metric('euclidean')
 
-            # TODO: Implement similarity calculation
-            similarity = 0
-            for column in columns_to_use:
-                if test_row[column] == train_row[column]:
-                    similarity += 1
+    for i in range(number_of_folds):
+        # Get training set by appending elements other than current fold
+        train_set = pandas.DataFrame()
+        for j in range(number_of_folds):
+            if j != i:
+                train_set = train_set.append(split_dfs[j])
 
-            current_similarities[similarity] = train_row
+        train_set_x = train_set.drop(['actual_productivity'], axis=1)
+        train_set_y = train_set['actual_productivity']
 
-        i = 1
-        for key in sorted(current_similarities.keys(), reverse=True):
-            predicted_productivity += float(current_similarities.get(key)['productivity'])
-            i += 1
-            if i == k_value:
-                break
+        test_set = split_dfs[i]
+        test_set_x = test_set.drop(['actual_productivity'], axis=1)
+        test_set_y = test_set['actual_productivity']
 
-        predicted_productivity = predicted_productivity / float(k_value)
-        print(predicted_productivity)
+        for test_index, test_row in test_set_x.iterrows():
 
+            actual_productivity = list()
+            actual_productivity.append(test_set_y[test_index])
+            current_distances = dict()
+
+            for train_index, train_row in train_set_x.iterrows():
+
+                first_point = numpy.array(train_row.values.tolist())
+                second_point = numpy.array(test_row.values.tolist())
+                current_distance = dist.pairwise(np.expand_dims(first_point, axis=0),
+                                                 np.expand_dims(second_point, axis=0))
+
+                current_distances[current_distance[0][0]] = train_set_y[train_index]
+
+            # Calculate weighed average of k nearest neighbors
+            i = 1
+            sum_of_productivity = 0
+            sum_of_weights = 0
+            for key in sorted(current_distances.keys(), reverse=True):
+                sum_of_productivity += float(current_distances.get(key)) / float(key)
+                sum_of_weights += 1.0 / float(key)
+                i += 1
+                if i == k_value:
+                    break
+
+            predicted_productivity = list()
+            predicted_productivity.append(sum_of_productivity / sum_of_weights)
+
+            # Calculate MSE
+            mse_list.append(mean_squared_error(actual_productivity, predicted_productivity))
+
+            # Calculate RMSE
+            rmse_list.append(mean_squared_error(actual_productivity, predicted_productivity, squared=False))
+
+            # Calculate MAPE
+            mape_list.append(mean_absolute_percentage_error(actual_productivity, predicted_productivity))
+
+    total_mse = sum(mse_list) / len(mse_list)
+    total_rmse = sum(rmse_list) / len(rmse_list)
+    total_mape = sum(mape_list) / len(mape_list)
+
+    return total_mse, total_rmse, total_mape
--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
-import pandas
+import time
+from enum import Enum
 import pandas as pd
-import numpy as np
-from sklearn import preprocessing
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-
+from sklearn.preprocessing import MinMaxScaler
 from src import knn, k_neighbors_regressor, decision_tree_regressor
 
-if __name__ == '__main__':
 
+class DistanceMetrics(Enum):
+    MANHATTAN_DISTANCE = 1
+    EUCLIDEAN_DISTANCE = 2
+
+
+if __name__ == '__main__':
     k_value = 5
     number_of_folds = 3
 
@@ -16,37 +19,77 @@ if __name__ == '__main__':
     print(df)
 
     # Eliminate not numeric and missing element columns for now
-    df_selected = df.drop(['date', 'quarter', 'department', 'day', 'wip'], axis=1)
+    df_selected = df.drop(['date', 'targeted_productivity', 'idle_time',
+                           'idle_men', 'no_of_style_change'], axis=1)
+    print(df_selected)
+
+    # TODO: Convert string valued columns to numeric values
+    df_selected['department'] = pd.to_numeric(df_selected['department'], errors='coerce')
+    df_selected['quarter'] = df_selected['quarter'].apply(lambda x: pd.factorize(x, sort=True))
+    df_selected['day'] = df_selected['day'].apply(lambda x: pd.factorize(x, sort=True))
+    print(df_selected)
+
+    # Fill empty values in column wip with mean of wip values
+    df_selected['wip'].fillna((df_selected['wip'].mean()), inplace=True)
     print(df_selected)
 
     # Apply min-max normalization
-    df_selected.loc[:, df_selected.columns != 'actual_productivity'] = MinMaxScaler().\
+    df_selected.loc[:, df_selected.columns != 'actual_productivity'] = MinMaxScaler(). \
         fit_transform(df_selected.loc[:, df_selected.columns != 'actual_productivity'])
     print(df_selected)
 
     # Shuffle elements of dataframe
-    shuffled_df = df_selected.sample(frac=1)
+    shuffled_df = df_selected.sample(frac=1).reset_index(drop=True)
     print(shuffled_df)
+
+    print("Running created knn algorithm with created similarity metric 1")
+
+    # Run knn with created similarity matrix 1 and measure performance
+    start_time = time.time()
+    knn_1_mse, knn_1_rmse, knn_1_mape = \
+        knn.measure_performance(shuffled_df, k_value, number_of_folds, DistanceMetrics.EUCLIDEAN_DISTANCE)
+    end_time = time.time()
+
+    print("MSE: ", knn_1_mse)
+    print("RMSE: ", knn_1_rmse)
+    print("MAPE: ", knn_1_mape)
+    print("Time passed: ", end_time - start_time, " seconds\n")
+
+    print("Running created knn algorithm with created similarity metric 2")
+
+    # Run knn with created similarity matrix 2 and measure performance
+    start_time = time.time()
+    knn_2_mse, knn_2_rmse, knn_2_mape = \
+        knn.measure_performance(shuffled_df, k_value, number_of_folds, DistanceMetrics.MANHATTAN_DISTANCE)
+    end_time = time.time()
+
+    print("MSE: ", knn_2_mse)
+    print("RMSE: ", knn_2_rmse)
+    print("MAPE: ", knn_2_mape)
+    print("Time passed: ", end_time - start_time, " seconds\n")
 
     print("Running KNeighborsRegressor with default settings")
 
     # Run KNeighborsRegressor from sklearn and measure performance
+    start_time = time.time()
     k_neighbors_regressor_mse, k_neighbors_regressor_rmse, k_neighbors_regressor_mape = \
         k_neighbors_regressor.measure_performance(shuffled_df, number_of_folds)
+    end_time = time.time()
 
     print("MSE: ", k_neighbors_regressor_mse)
     print("RMSE: ", k_neighbors_regressor_rmse)
-    print("MAPE: ", k_neighbors_regressor_mape, "\n")
+    print("MAPE: ", k_neighbors_regressor_mape)
+    print("Time passed: ", end_time - start_time, " seconds")
 
-    print("Running DecisionTreeRegressor with default settings")
+    print("Running DecisionTreeRegressor with default settings\n")
 
     # Run DecisionTreeRegressor from sklearn and measure performance
+    start_time = time.time()
     decision_tree_regressor_mse, decision_tree_regressor_rmse, decision_tree_regressor_mape = \
         decision_tree_regressor.measure_performance(shuffled_df, number_of_folds)
+    end_time = time.time()
 
     print("MSE: ", decision_tree_regressor_mse)
     print("RMSE: ", decision_tree_regressor_rmse)
-    print("MAPE: ", decision_tree_regressor_mape, "\n")
-
-    # Run knn and measure performance
-    # knn.measure_performance(rest, split_dfs[i], k_value, columns_to_use)
+    print("MAPE: ", decision_tree_regressor_mape)
+    print("Time passed: ", end_time - start_time, " seconds\n")
